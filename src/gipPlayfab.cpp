e3323b66f0b651c9b760ab8e87e5da5cf56f1341
celenk <celenk@celenko>
2022-08-18 17:38:00 +0300

    gipPlayfab.cpp added

--- src/gipPlayfab.cpp
+++ src/gipPlayfab.cpp
@@ -3,14 +3,31 @@
  *
  *  Created on: 13 Sep 2021
  *      Author: noyan
+ *      		Sinem Algul
+*      			Furkan Celenk
  */
 
 #include "gipPlayfab.h"
 
+bool gipPlayfab::ticketidreceived = false;
+bool gipPlayfab::ticketcanceled = false;
+std::string gipPlayfab::myid;
+std::string gipPlayfab::mytitleid;
+std::string gipPlayfab::myentitytype;
+std::string gipPlayfab::matchid;
+std::string gipPlayfab::myqueuename;
+PlayFab::MultiplayerModels::CreateMatchmakingTicketResult gipPlayfab::createticketresult;
+
 bool gipPlayfab::finished = false;
-const std::string gipPlayfab::titleID = "3F336";
+const std::string gipPlayfab::titleID = "6AFF4";
 
 gipPlayfab::gipPlayfab() {
+	myid = "";
+	mytitleid = "";
+	myentitytype = "";
+	matchid = "";
+	myqueuename = "TestQueue";
+	ticketidreceived = false;
 	PlayFabSettings::staticSettings->titleId = titleID;
 }
 
@@ -39,24 +56,143 @@ void gipPlayfab::login(const std::string& username, const std::string& password)
 		sleep(1);
 }
 
+void gipPlayfab::createMatchmakingTicket(const int& giveupafterseconds) {
+	CreateMatchmakingTicketRequest request;
+	MatchmakingPlayer creator;
+	MultiplayerModels::EntityKey entity;
+	MatchmakingPlayerAttributes attributes;
+	Json::Value dataObject;
+
+	entity.Id = mytitleid;
+	entity.Type = myentitytype;
+
+	dataObject["Skill"] = 24.4;
+	attributes.DataObject = dataObject;
+
+	creator.Entity = entity;
+	creator.Attributes = attributes;
+
+	request.Creator = creator;
+	request.GiveUpAfterSeconds = giveupafterseconds;
+	request.QueueName = myqueuename;
+
+	PlayFabMultiplayerAPI::CreateMatchmakingTicket(request, OnMatchmakingTicketCreated, OnMatchmakingTicketFail);
+
+	while (PlayFabMultiplayerAPI::Update() != 0)
+			sleep(1);
+}
+
+PlayFab::MultiplayerModels::GetMatchmakingTicketResult gipPlayfab::checkMatchmakingTicket(const std::string& ticketid) {
+
+	 	while (true){
+		GetMatchmakingTicketRequest request;
+
+		request.TicketId = ticketid;
+		request.QueueName = myqueuename;
+
+		PlayFabMultiplayerAPI::GetMatchmakingTicket(request, OnCheckMatchmakingTicket, OnCheckMatchmakingFail);
+
+		while (PlayFabMultiplayerAPI::Update() != 0)
+					sleep(1);
+
+		sleep(6);
+		}
+}
+
+void gipPlayfab::joinMatch() {
+	GetMatchRequest request;
+
+	request.MatchId = matchid;
+	request.QueueName = myqueuename;
+
+	PlayFabMultiplayerAPI::GetMatch(request, OnGetMatchResult, OnGetMatchFail);
+
+	while (PlayFabMultiplayerAPI::Update() != 0)
+						sleep(1);
+}
+
+void gipPlayfab::canceledMatch() {
+	CancelMatchmakingTicketRequest request;
+
+	request.QueueName = myqueuename;
+	request.TicketId = createticketresult.TicketId;
+
+	PlayFabMultiplayerAPI::CancelMatchmakingTicket(request, OnTicketCanceled, OnTicketCanceledFail);
+
+	while (PlayFabMultiplayerAPI::Update() != 0)
+							sleep(1);
+}
+
+void gipPlayfab::OnTicketCanceled(const CancelMatchmakingTicketResult& result, void* customData) {
+	gLoge("gipPlayfab") << "Match Canceled.";
+	ticketcanceled = true;
+}
+
+void gipPlayfab::OnTicketCanceledFail(const PlayFabError& error, void* customData) {
+	gLoge("gipPlayfab") << error.GenerateErrorReport();
+}
+
+void gipPlayfab::OnGetMatchResult(const GetMatchResult& result, void* customData) {
+//	result.pfServerDetails->IPV4Address;
+}
+
+void gipPlayfab::OnGetMatchFail(const PlayFabError& error, void* customData) {
+	gLoge("gipPlayfab") << "joinMatch failed. \n";
+	gLoge("gipPlayfab") << error.GenerateErrorReport();
+}
+
+void gipPlayfab::OnCheckMatchmakingTicket(const GetMatchmakingTicketResult& result, void* customData) {
+    matchid = result.MatchId;
+    gLogi("gipPlayfab") << "Matchmaking Ticket created. \n" << "Ticket Status: " << result.Status << " --- " << "MatchID:" << matchid;
+}
+
+void gipPlayfab::OnCheckMatchmakingFail(const PlayFabError& error, void* customData) {
+    gLoge("gipPlayfab") << "Matchmaking failed.\n";
+    gLoge("gipPlayfab") << "Here's some debug information:\n";
+    gLoge("gipPlayfab") << error.GenerateErrorReport();
+}
+
+void gipPlayfab::OnMatchmakingTicketCreated(const CreateMatchmakingTicketResult& result, void* customData) {
+    gLogi("gipPlayfab") << "Matchmaking Ticket created. " << "Your Ticket ID is: " << result.TicketId;
+    createticketresult = result;
+    ticketidreceived = true;
+}
+void gipPlayfab::OnMatchmakingTicketFail(const PlayFabError& error, void* customData) {
+    gLoge("gipPlayfab") << "Problem occurred while creating matchmaking ticket.\n";
+    gLoge("gipPlayfab") << "Here's some debug information:\n";
+    gLoge("gipPlayfab") << error.GenerateErrorReport();
+}
+
 void gipPlayfab::OnLoginSuccess(const LoginResult& result, void* customData) {
-	gLogi("gipPlayfab") << "Login successful.";
+	gLogi("gipPlayfab") << "Login successful. " << "Session Information: " << result.SessionTicket << " \n";
+	myid = result.PlayFabId;
+	mytitleid = result.EntityToken->Entity->Id;
+	myentitytype = result.EntityToken->Entity->Type;
+//	gLogi("gipPlayfab") << "myid:" << myid;
 	finished = true;
 }
 
 void gipPlayfab::OnLoginFail(const PlayFabError& error, void* customData) {
-	gLogi("gipPlayfab") << "Something went wrong with your first API call.\n";
-	gLogi("gipPlayfab") << "Here's some debug information:\n";
-	gLogi("gipPlayfab") << error.GenerateErrorReport();
+	gLoge("gipPlayfab") << "Something went wrong with your first API call.\n";
+	gLoge("gipPlayfab") << "Here's some debug information:\n";
+	gLoge("gipPlayfab") << error.GenerateErrorReport();
 	finished = true;
 }
 
 void gipPlayfab::OnRegisterSuccess(const RegisterPlayFabUserResult& result, void* customData) {
-	gLogi("gipPlayfab") << "Register Success usr: " << result.Username << " id: " << result.PlayFabId;
+	gLogi("gipPlayfab") << "Register Success user: " << result.Username << " id: " << result.PlayFabId;
 }
 
 void gipPlayfab::OnRegisterFail(const PlayFabError& error, void* customData) {
-	gLogi("gipPlayfab") << "Something went wrong with register.\n";
-	gLogi("gipPlayfab") << "Here's some debug information:\n";
-	gLogi("gipPlayfab") << error.GenerateErrorReport();
+	gLoge("gipPlayfab") << "Something went wrong with register.\n";
+	gLoge("gipPlayfab") << "Here's some debug information:\n";
+	gLoge("gipPlayfab") << error.GenerateErrorReport();
+}
+
+bool gipPlayfab::isTicketIdReceived() {
+	return ticketidreceived;
+}
+
+bool gipPlayfab::isTicketCanceled() {
+	return ticketcanceled;
 }
--- src/gipPlayfab.h
+++ src/gipPlayfab.h
@@ -3,6 +3,10 @@
  *
  *  Created on: 13 Sep 2021
  *      Author: noyan
+ *      		Sinem Algul
+ *      		Furkan Celenk
+ *
+ *
  */
 
 #ifndef SRC_GIPPLAYFAB_H_
@@ -13,11 +17,21 @@
 #include "playfab/PlayFabClientApi.h"
 #include "playfab/PlayFabSettings.h"
 #include "playfab/PlayFabApiSettings.h"
+#include "playfab/PlayFabMultiplayerDataModels.h"
+#include "playfab/PlayFabMultiplayerApi.h"
+#include "playfab/PlayFabDataDataModels.h"
+#include "playfab/PlayFabAuthenticationApi.h"
+#include "playfab/PlayFabAuthenticationContext.h"
+#include "playfab/PlayFabProfilesApi.h"
+#include "playfab/PlayFabProfilesDataModels.h"
 #include <unistd.h>
-
 
 using namespace PlayFab;
 using namespace ClientModels;
+using namespace ProfilesModels;
+using namespace MultiplayerModels;
+using namespace DataModels;
+using namespace AuthenticationModels;
 
 
 class gipPlayfab : public gBasePlugin{
@@ -25,20 +39,42 @@ public:
 	gipPlayfab();
 	virtual ~gipPlayfab();
 
+	//
 	void registerPlayFabAccount(const std::string& username, const std::string& email, const std::string& password, const std::string& displayName);
 	void login(const std::string& username, const std::string& password);
+	void createMatchmakingTicket(const int& giveupafterseconds);
+	PlayFab::MultiplayerModels::GetMatchmakingTicketResult checkMatchmakingTicket(const std::string& ticketid);
+	void joinMatch();
+	void canceledMatch();
 
 	void openLobby();
+	bool isTicketIdReceived();
+	bool isTicketCanceled();
+	static PlayFab::MultiplayerModels::CreateMatchmakingTicketResult createticketresult;
 
 private:
 	static void OnLoginSuccess(const LoginResult& result, void* customData);
 	static void OnLoginFail(const PlayFabError& error, void* customData);
 	static void OnRegisterSuccess(const RegisterPlayFabUserResult& result, void* customData);
 	static void OnRegisterFail(const PlayFabError& error, void* customData);
+	static void OnMatchmakingTicketCreated(const CreateMatchmakingTicketResult& result, void* customData);
+	static void OnMatchmakingTicketFail(const PlayFabError& error, void* customData);
+	static void OnCheckMatchmakingTicket(const GetMatchmakingTicketResult& result, void* customData);
+	static void OnCheckMatchmakingFail(const PlayFabError& error, void* customData);
+	static void OnGetMatchResult(const GetMatchResult& result, void* customData);
+	static void OnGetMatchFail(const PlayFabError& error, void* customData);
+	static void OnTicketCanceled(const CancelMatchmakingTicketResult& result, void* customData);
+	static void OnTicketCanceledFail(const PlayFabError& error, void* customData);
 
 	static bool finished;
-
+	static bool ticketidreceived;
+	static bool ticketcanceled;
 	static const std::string titleID;
+	static std::string myid;
+	static std::string mytitleid;
+	static std::string myentitytype;
+	static std::string matchid;
+	static std::string myqueuename;
 };
 
 #endif /* SRC_GIPPLAYFAB_H_ */
